1b4c59b9930bf02607b9e15ca95507bfbd7a5fde
Gabriel Leandro De Moraes Santiago <gabrielleandrosantiago9@gmail.com>
2022-11-04 17:19:52 -0300

    g

--- index.php
+++ index.php
@@ -1,33 +1,82 @@
 <?php 
- //declarando variáveis
-$anon = 1989;
-$anoa = 2022;
+//comentario linha
+# comentario de linha
+/* comentario*/
 
- $idade = $anoa - $anon; 
+// declaracao de variaveis e matrizes - manipulação de data/hr - manipulação de stringer
+$nome = "gabriel";
+$dataNasc = date('2003/05/20');
+echo($nome.' - '. $dataNasc);
+echo('<br>');
+$hoje = date('d/m/Y H:i:s');
+echo('<br>');
+
+        //definindo timezone
+date_default_timezone_set('America/Sao_Paulo');
 
 
-//$idade = "Dezenove";
-//$idade = 19.5
-echo("<h3> A idade do pião é: ".$idade." </h3>");
+$data = new DateTime();
+echo('<br>');
+//print_r($data);
 
- $times = array();
-$times[0] = "palmeiras";
-$times[1] = "internacional";
-$times[2] = "flamengo";
-echo("<br>");
-print_r($times);
-echo("<br>");
-foreach ($times as $times) {
-    echo($times. "<br>");
+$hoje = date('d/m/Y H:i:s'); echo($hoje.'<br>');
+$hoje = date('d-m-Y H:i:s'); echo($hoje.'<br>');
+$hoje = date('D, d M Y'); echo($hoje.'<br>');
+$hoje = date('M H:i:s'); echo($hoje.'<br>');
+$hoje = date('D-d H:i'); echo($hoje.'<br>');
+$hoje = date('d/m/Y H:i:s'); echo($hoje.'<br>');
+$teste = true;
+$idade = 35;
+$valor = 458.65;
+$grana = 0;
+// estrutura  de controle de decisão
+if (!($grana>0)) { // caso retorne verdadeiro
+    echo('Sacar grana!<br>');
+}
+//estrutura de repetição
+
+// funções e configurações
+if(isset($_GET['enviar'])){
+$nome = $_GET['txt-nome'];
+$email = $_GET['txt-email'];
+$data_nasc = $_GET['txt-data-nasc'];
+$dataT = new Datetime($data_nasc);
+$data_nasc = date_format($dataT,'d-M-Y');
+
+$aluno =array('nome'=>$nome,'email'=>$email, 'datan'=>$data_nasc);
+
+foreach($aluno as $key => $value){
+    echo('<h2>'.$key.': '. $value.'</h2>');
+}
+0
+// echo('<h1>'.$valor.'</h1>');
 }
 ?>
-<table>
-    <th>Id</th>
-    <th>nome</th>
-    <?php foreach ($times as $Id => $nome) { ?>
-        <tr>
-            <td><?php echo($Id + 1);?> </td>;
-            <td><?php echo($nome + 1);?> </td>;
-        </tr>
-    <?php } ?>
-</table>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta http-equiv="refresh" content="1"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="get">
+        <label for="txt-nome">
+            nome:
+            <input type="text" name="txt-nome" placeholder="digite o nome..." required>
+        </label> <br>
+<label for="">
+    email:
+    <input type="text" name="txt-email" placeholder="digite o email..." required>
+</label> <br>
+<label for="">
+    Data Nascimento:
+<input type="date" name="txt-data-nasc" required>
+</label> <br>
+        <button type="submit" name=enviar>Enviar</button>
+
+    </form>
+</body>
+</html>
